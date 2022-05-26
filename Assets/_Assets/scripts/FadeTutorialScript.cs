5d1298fcc672a27f0f1238c982c05d6b5461bd83
Your Name <yohai109@gmail.com>
2022-05-26 22:15:39 +0300

    fix tutorial

--- Assets/_Assets/scripts/FadeTutorialScript.cs
+++ Assets/_Assets/scripts/FadeTutorialScript.cs
@@ -5,40 +5,46 @@ using UnityEngine.UI;
 
 public class FadeTutorialScript : MonoBehaviour
 {
-    public Canvas tutorialCanvas;
+    //public Canvas tutorialCanvas;
     public Text tutorialText;
-    public GameObject tutorialPanel;
+    //public GameObject tutorialPanel;
     private float fadeSpeed = 5;
     private bool entrenace = false;
     // Start is called before the first frame update
 
-    private void Start() {
+    private void Start()
+    {
         tutorialText.color = Color.clear;
     }
 
-    private void Update() {
+    private void Update()
+    {
         if (entrenace)
         {
             tutorialText.color = Color.Lerp(tutorialText.color, Color.white, fadeSpeed * Time.deltaTime);
-            tutorialPanel.SetActive(true);
+            tutorialText.enabled = true;
+            //tutorialPanel.SetActive(true);
 
         }
         else
         {
             tutorialText.color = Color.Lerp(tutorialText.color, Color.clear, fadeSpeed * Time.deltaTime);
-            tutorialPanel.SetActive(false);
+            tutorialText.enabled = false;
+            //tutorialPanel.SetActive(false);
 
-        }    
+        }
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.gameObject.tag == "Player")
         {
             entrenace = true;
         }
     }
 
-    private void OnTriggerExit(Collider other) {
+    private void OnTriggerExit(Collider other)
+    {
         if (other.gameObject.tag == "Player")
         {
             entrenace = false;
