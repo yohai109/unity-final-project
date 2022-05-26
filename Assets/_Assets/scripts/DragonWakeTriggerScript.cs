d7bf270007a350bfe9b4a881d3370129b16a5af0
Your Name <yohai109@gmail.com>
2022-05-26 22:45:24 +0300

    add gate ate the finish line

--- Assets/_Assets/scripts/DragonWakeTriggerScript.cs
+++ Assets/_Assets/scripts/DragonWakeTriggerScript.cs
@@ -6,7 +6,7 @@ public class DragonWakeTriggerScript : MonoBehaviour
 {
     public DragonScript dragon;
     public CanvasScript canvas;
-    private static string textBoxText = "RUN" ;
+    private static string textBoxText = "RUN";
     private bool dragonGotAwake = false;
     // Start is called before the first frame update
     void Start()
@@ -19,8 +19,8 @@ public class DragonWakeTriggerScript : MonoBehaviour
     {
         RaycastHit hit;
         int rayLength = 50;
-        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z), Vector3.left * rayLength, Color.green);
-        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z), Vector3.left, out hit, rayLength))
+        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Vector3.left * rayLength, Color.green);
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Vector3.left, out hit, rayLength))
         {
             Debug.Log("object entered ray");
             if (hit.collider.gameObject.tag == "Player" && !dragonGotAwake)
