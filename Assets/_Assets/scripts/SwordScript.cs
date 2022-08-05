7ef256ed8695e538b3a5ae0bcad57063100e07a6
Your Name <yohai109@gmail.com>
2022-08-05 12:47:01 +0300

    add sword to the user. start adding AI. add gate to block exit

---
+++ Assets/_Assets/scripts/SwordScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("hit");
+        }
+        /*if (Input.GetMouseButtonUp(0))
+        {
+            gameObject.GetComponent<Animator>().ResetTrigger("hit");
+        }*/
+    }
+}
