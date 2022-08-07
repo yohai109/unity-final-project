1de5bdf91616bb952ee7197e25b667aae84ca09a
Your Name <yohai109@gmail.com>
2022-08-07 20:50:06 +0300

    sword is now killing dragons

--- Assets/_Assets/scripts/WeaponScripts/SwordScript.cs
+++ Assets/_Assets/scripts/WeaponScripts/SwordScript.cs
@@ -16,6 +16,26 @@ public class SwordScript : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             gameObject.GetComponent<Animator>().SetTrigger("hit");
+            RaycastHit hit;
+            int rayLength = 50;
+            if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z), Vector3.forward, out hit, rayLength))
+            {
+                if (hit.collider.gameObject.tag.Equals("Minion"))
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+
+            }
+
         }
     }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Minion"))
+        {
+            Destroy(other.gameObject);
+            *//*Destroy(gameObject);*//*
+        }
+    }*/
 }
