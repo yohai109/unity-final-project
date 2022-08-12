0cdbfe71695c9e3c234ca35ce68437d29c4975a1
Yoav Zinger <47221812y@gmail.com>
2022-08-12 17:39:45 +0300

    fixing sword tutorial

--- Assets/_Assets/scripts/TutorialManagerScript.cs
+++ Assets/_Assets/scripts/TutorialManagerScript.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class TutorialManagerScript : MonoBehaviour
 {
     public int patrolSpeed;
+
+    public GameObject invisibleWall_Bow;
+    public GameObject invisibleWall_Sword;
+
+    public GameObject playerBow;
+    public GameObject playerSword;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBow.SetActive(false);
+        playerSword.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayerTookSword() {
+        GameObject sword = GameObject.FindWithTag("Sword");
+        Destroy(sword);
+        Destroy(invisibleWall_Sword);
+        playerSword.SetActive(true);
+        playerBow.SetActive(false);
     }
 }
