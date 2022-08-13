4f80f305c1a39fc49c3001dfdb81674d7f02648e
Yoav Zinger <47221812y@gmail.com>
2022-08-13 19:28:42 +0300

    added invisible walls

--- Assets/_Assets/scripts/TutorialManagerScript.cs
+++ Assets/_Assets/scripts/TutorialManagerScript.cs
@@ -8,6 +8,8 @@ public class TutorialManagerScript : MonoBehaviour
 
     public GameObject invisibleWall_Bow;
     public GameObject invisibleWall_Sword;
+    public GameObject invisibleWall_Target;
+    public GameObject invisibleWall_Dragon;
 
     public GameObject playerBow;
     public GameObject playerSword;
@@ -16,6 +18,8 @@ public class TutorialManagerScript : MonoBehaviour
 
     public GameObject target;
     private int animationStage;
+
+    private bool isDragonActive = false;
 
 
     // Start is called before the first frame update
@@ -33,7 +37,11 @@ public class TutorialManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isDragonActive) {
+            if(GameObject.FindGameObjectWithTag("Minion") == null) {
+                Destroy(invisibleWall_Dragon);
+            }
+        }
     }
 
     public void PlayerTookBow() {
@@ -53,5 +61,11 @@ public class TutorialManagerScript : MonoBehaviour
         dragon.SetActive(true);
         playerSword.SetActive(true);
         playerBow.SetActive(false);
+
+        isDragonActive = true;
+    }
+
+    public void TargetDestroyed() {
+        Destroy(invisibleWall_Target);
     }
 }
